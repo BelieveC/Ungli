faa8ce483f39b6d8ef3f2ec17809efd7b2719d03
BelieveC <cayadav2009@gmail.com>
2016-09-11 05:15:46 +0530

    Configure sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+	def create
+		user = User.from_omniauth(env["omniauth.auth"])
+	    session[:user_id] = user.id
+	    redirect_to root_path,notice:" #{user.name} you are logged in. Have a great Time at Ungli"
+	end
+
+	def destroy
+	   @name = current_user.name
+	   session[:user_id] = nil
+	   redirect_to :back, notice:"#{@name} you are logged out. Thank you for Raising voice against corruption!"
+	end
 end

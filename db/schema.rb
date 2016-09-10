4d84b584f25afbc2672c9c82a7942d7e0e8ad9e4
BelieveC <cayadav2009@gmail.com>
2016-09-11 05:05:35 +0530

    Add user model to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910231625) do
+ActiveRecord::Schema.define(version: 20160910233419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 20160910231625) do
     t.string   "simage_content_type"
     t.integer  "simage_file_size"
     t.datetime "simage_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "age"
+    t.string   "region"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

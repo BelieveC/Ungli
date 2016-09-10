8ba9a82d419bf70c94e7a1277620972c0d1ef42c
BelieveC <cayadav2009@gmail.com>
2016-09-11 05:16:45 +0530

    Add routes for facebook oauth

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
     root "stories#index"
 
     get "/contact"=>"abouts#contact",as:"contact"
+     match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+	 match 'auth/failure', to: redirect('/'), via: [:get, :post]
+	 match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
     resources :stories
 end

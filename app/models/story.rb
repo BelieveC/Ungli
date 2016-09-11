b653eeea7d4cac69331ccc2134df5ad678de6b45
BelieveC <cayadav2009@gmail.com>
2016-09-11 12:35:50 +0530

    Remove Associates relations

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,7 +1,5 @@
 class Story < ActiveRecord::Base
 	acts_as_votable
-	has_many :associates
-	has_many :users,through: :associates
 	belongs_to :user
 	is_impressionable
 	has_attached_file :simage, styles: { medium: "300x300>"}, default_url: "/images/:style/missing.png"

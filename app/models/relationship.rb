8daf04d1a172382fd641fa7a7e057caa227d2d85
BelieveC <cayadav2009@gmail.com>
2016-09-11 11:08:36 +0530

    Add association to relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,4 @@
 class Relationship < ActiveRecord::Base
+	belongs_to :supporter,class_name: "User"
+	belongs_to :backed, class_name: "Story"
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,6 @@
 class Story < ActiveRecord::Base
 	acts_as_votable
+	has_many :supporter,through: :relationship
 	belongs_to :user
 	is_impressionable
 	has_attached_file :simage, styles: { medium: "300x300>"}, default_url: "/images/:style/missing.png"

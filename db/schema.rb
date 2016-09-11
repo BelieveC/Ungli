beac37665613a543c69689fd38e1bba1de57d1c1
BelieveC <cayadav2009@gmail.com>
2016-09-11 06:03:36 +0530

    Add uid, provider to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910233419) do
+ActiveRecord::Schema.define(version: 20160911003303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,12 @@ ActiveRecord::Schema.define(version: 20160910233419) do
     t.string   "name"
     t.integer  "age"
     t.string   "region"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "uid"
+    t.string   "provider"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
   end
 
 end

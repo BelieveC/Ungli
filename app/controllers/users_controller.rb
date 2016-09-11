c4094761e78edfe38ca2b743bd59afb0369368a2
BelieveC <cayadav2009@gmail.com>
2016-09-11 06:41:39 +0530

    Add user view and contrller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+	def show
+		@user = User.find(params[:id])
+	end
+	
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(users_params)
+			redirect_to @user
+		else
+			render "Edit"
+		end
+	end
+
+	private
+
+		def users_params
+			params.require(:user).permit(:name,:age,:region)
+		end
 end

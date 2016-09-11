ebc27013bf35ab3d229f3bba1dda7c2035f64fe4
BelieveC <cayadav2009@gmail.com>
2016-09-11 08:35:59 +0530

    Add Association for Story Model

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,6 @@
 class Story < ActiveRecord::Base
+	
+	has_one :user
 	is_impressionable
 	has_attached_file :simage, styles: { medium: "300x300>"}, default_url: "/images/:style/missing.png"
  	validates_attachment_content_type :simage, content_type: /\Aimage\/.*\z/
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,13 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+   config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_protocol => 'http',
+    :s3_credentials => {
+      :bucket => "bitnotes",
+      :access_key_id =>"AKIAIT2FFKMXGD3AGIBA",
+      :secret_access_key => "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"
+    }
+  }
 end

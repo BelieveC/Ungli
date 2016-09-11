35dd3faf5cd8733609e86a24d0a35f85441985d3
BelieveC <cayadav2009@gmail.com>
2016-09-11 06:51:11 +0530

    Add avatar for user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911003303) do
+ActiveRecord::Schema.define(version: 20160911012043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,12 +56,16 @@ ActiveRecord::Schema.define(version: 20160911003303) do
     t.string   "name"
     t.integer  "age"
     t.string   "region"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.string   "uid"
     t.string   "provider"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end

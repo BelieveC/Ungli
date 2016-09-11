6f9067dc12f1ad9c92d6c058baf9248651885b6f
BelieveC <cayadav2009@gmail.com>
2016-09-11 09:11:23 +0530

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
+  helper_method :current_user?
+  helper_method :authenticate_user!
 
   def current_user
   	 @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def current_user?(other_user)
+    if other_user.id == current_user.id
+      true
+    else
+      false
+    end
+  end
+
+  def authenticate_user!
+    if session[:user_id]
+      return true
+    else
+      redirect_to :back,notice:"Please Sign In before you Contribute!"
+   	end
   end
   
 end

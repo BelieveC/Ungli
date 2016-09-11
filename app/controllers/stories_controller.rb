6ece5d3916fdd9b35d51fad2ab0dde7bcf51247e
BelieveC <cayadav2009@gmail.com>
2016-09-11 09:11:50 +0530

    Add auth to stories action

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,6 +2,7 @@ class StoriesController < ApplicationController
 
 	impressionist :actions=>[:show]
 	before_action :get_story,only:[:show,:edit,:update,:destroy]
+	before_action :authenticate_user!,only:[:edit,:new,:create,:update,:destroy]
 
 	def index
 		@stories = Story.all.order("created_at DESC")
@@ -15,13 +16,21 @@ class StoriesController < ApplicationController
 	end
 
 	def edit
+		unless current_user.id == @story.user.id
+			redirect_to :back,notice:"Your don't have rights to edit this Story"
+		end
+
 	end
 
 	def update
-		if @story.update(story_params)
-			redirect_to @story,notice: "Successfully updated your story"
+		unless current_user.id == @story.user.id
+			redirect_to :back,notice:"Your don't have rights to update this Story"
 		else
-			render "edit"
+			if @story.update(story_params)
+				redirect_to @story,notice: "Successfully updated your story"
+			else
+				render "edit"
+			end
 		end
 	end
 
@@ -36,6 +45,12 @@ class StoriesController < ApplicationController
 	end
 
 	def destroy
+		unless current_user.id == @story.user.id
+			redirect_to :back,notice:"Your don't have rights to Delete this Story"
+		else
+			@story.destroy
+			redirect_to root_path,notice:"Successfully removed your story"
+		end
 	end
 
  	private
